483dab9cc51bc33e36a29af2af924320bf7f6e16
abrazax56 <ahmadbenirusli@gmail.com>
2024-03-25 15:34:46 +0700

    masih hari keduabelas, kamu apa kabar?

---
+++ dist/src/Jawa.js
@@ -0,0 +1,7 @@
+"use strict";
+class ꦥꦼꦂꦱꦺꦴꦤ꧀ {
+    constructor(ꦠ꧀ꦪ꧀ꦥꦼ) {
+        this.ꦠ꧀ꦪ꧀ꦥꦼ = ꦠ꧀ꦪ꧀ꦥꦼ;
+    }
+}
+const ꦩꦼ = new ꦥꦼꦂꦱꦺꦴꦤ꧀({ ꦤꦩꦺ: "Ahmad Beni Rusli" });
---
+++ src/Jawa.ts
@@ -0,0 +1,7 @@
+interface ꦲꦸꦩꦤ꧀ {
+  ꦤꦩꦺ: string;
+}
+class ꦥꦼꦂꦱꦺꦴꦤ꧀<ꦠ꧀ꦪ꧀ꦥꦼ extends ꦲꦸꦩꦤ꧀> {
+  constructor(public ꦠ꧀ꦪ꧀ꦥꦼ: ꦲꦸꦩꦤ꧀) {}
+}
+const ꦩꦼ = new ꦥꦼꦂꦱꦺꦴꦤ꧀<ꦲꦸꦩꦤ꧀>({ꦤꦩꦺ: "Ahmad Beni Rusli"});
